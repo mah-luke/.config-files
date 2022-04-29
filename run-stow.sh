51d749ffe890172b18e90e509d217bf29999b3c1
Lukas Mahler <lukas.mah@outlook.at>
2022-04-30 00:21:44 +0200

    add pulse, move etc-apt to etc folder

--- run-stow.sh
+++ run-stow.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# --- DEFAULT CONFIG ---
+
 # correct path even if script was called via sudo
 DIR="$(eval echo ~${SUDO_USER})/.config-files"
 
 stow --dir=$DIR --target=$HOME home
-sudo stow --dir=$DIR --target=/etc/apt etc-apt
+sudo stow --dir=$DIR --target=/etc etc
+
+
+# --- DEVICE SPECIFIC ---
