e694635882df03d6b552ab279002ceb4b7122585
Lukas <lukas.mah@outlook.at>
2022-05-26 18:17:28 +0200

    add --adopt option in stow command

--- run-stow.sh
+++ run-stow.sh
@@ -5,9 +5,12 @@
 # correct path even if script was called via sudo
 DIR="$(eval echo ~${SUDO_USER})/.config-files"
 
-stow --dir=$DIR --target=$HOME home
-sudo stow --dir=$DIR --target=/etc etc
 
+stow --dir=$DIR --target=$HOME home --adopt
+sudo stow --dir=$DIR --target=/etc etc --adopt
+
+# Remove changes caused by adopt
+git reset --hard
 
 # --- DEVICE SPECIFIC ---
 
