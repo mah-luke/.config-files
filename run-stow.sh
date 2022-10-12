eab580c6591e3a7197266a6beb0adc73e7a8dca3
Lukas Mahler <lukas.mah@outlook.at>
2022-10-12 09:52:52 +0200

    remove typo

--- run-stow.sh
+++ run-stow.sh
@@ -7,7 +7,7 @@ DIR="$(eval echo ~"${SUDO_USER}")/.config-files"
 
 git stash
 
-stow --dir="$DIR" --target="$SHOME" home --adopt
+stow --dir="$DIR" --target="$HOME" home --adopt
 sudo stow --dir="$DIR" --target=/etc etc --adopt
 
 # Remove changes caused by adopt
