c3852b7acde9983533b78491656da274fb7f5ffe
Lukas Mahler <lukas.mah@outlook.at>
2022-10-12 14:14:12 +0200

    add onedrive config

--- run-stow.sh
+++ run-stow.sh
@@ -9,6 +9,8 @@ git stash
 
 stow --dir="$DIR" --target="$HOME" home --adopt
 sudo stow --dir="$DIR" --target=/etc etc --adopt
+sudo stow --dir="$DIR" --target=/usr usr --adopt
+
 
 # Remove changes caused by adopt
 git reset --hard
