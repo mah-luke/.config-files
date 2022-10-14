e2f97b88ad0565ca32e6bd766e3f2c6c82e7afee
Lukas <lukas.mah@outlook.at>
2022-10-14 18:57:44 +0200

    update aliases & environment, add bac

--- home/.config/shell/aliases.sh
+++ home/.config/shell/aliases.sh
@@ -3,6 +3,7 @@
 
 # --- uni ---
 alias sem='cd $HOME/OneDrive-TU/Uni/7.Semester'
+alias bac='cd $HOME/OneDrive-TU/Uni/Bachelorarbeit'
 
 # ---- application ---
 alias v="nvim"
@@ -20,11 +21,3 @@ alias .2="cd ../.."
 alias .3=".2 && cd .."
 alias .4=".3 && cd .."
 alias .5=".4 && cd .."
-
-alias update="sudo rm -rf /var/lib/dpkg/lock-frontend &&
-                sudo rm -rf /var/lib/dpkg/lock &&
-                sudo apt-get update && 
-                sudo apt-get upgrade -y &&
-                sudo apt-get dist-upgrade -y &&
-                sudo apt-get autoremove -y &&
-                sudo apt-get autoclean -y"
--- home/.config/shell/environment.sh
+++ home/.config/shell/environment.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 
 # add keyring / ssh key support
-export SSH_AUTH_SOCK=/run/user/$(id -u)/keyring/ssh
+SSH_AUTH_SOCK=/run/user/"$(id -u)"/keyring/ssh
+export SSH_AUTH_SOCK
 
 # Use nvim as editor
 export VISUAL=/usr/bin/nvim
 export EDITOR="$VISUAL"
 
-export sem=$HOME/OneDrive-TU/Uni/6.Semester
+export sem="$HOME"/OneDrive-TU/Uni/7.Semester
+export bac="$HOME"/OneDrive-TU/Uni/Bachelorarbeit
 
 export sse_pw=abebohw5EeWietha6soK
 export sse=~/Programming/uni/sse/Sec4SystemsEngineering_lab1
