240652c3b84825e0f9d93d7976a9583a693d8a41
Lukas <lukas.mah@outlook.at>
2023-03-16 19:29:36 +0100

    hardcode zsh_custom env variable in setup file

--- setup/oh-my-zsh.sh
+++ setup/oh-my-zsh.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
+ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
 git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "${ZSH_CUSTOM}/plugins/autoswitch_virtualenv"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
