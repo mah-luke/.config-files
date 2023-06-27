b747283e6c31f1aa7036544c4af0947c1e948045
Lukas <lukas.mah@outlook.at>
2023-06-27 10:20:09 +0200

    comment out /etc for stow

--- run-stow.sh
+++ run-stow.sh
@@ -8,7 +8,7 @@ DIR="$(eval echo ~"${SUDO_USER}")/.config-files"
 git stash
 
 stow --dir="$DIR" --target="$HOME" home --adopt
-sudo stow --dir="$DIR" --target=/etc etc --adopt
+# sudo stow --dir="$DIR" --target=/etc etc --adopt
 sudo stow --dir="$DIR" --target=/usr usr --adopt
 
 
