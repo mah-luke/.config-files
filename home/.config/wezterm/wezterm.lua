47a2a30aa1283028bae0552be41643019ffab47a
Lukas <lukas.mah@outlook.at>
2023-07-06 23:39:14 +0200

    improve wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -12,7 +12,46 @@ config.window_background_opacity = 0.99
 
 config.colors = {
    foreground = "#AFB5C1", -- default: #abb2bf
-   background = "#161a1d",
+   background = "#161a1d", -- default: #282c34
+
+   -- Overrides the cell background color when the current cell is occupied by the
+   -- cursor and the cursor style is set to Block
+   -- cursor_bg = "hsl(219, 14%, 30%)",
+   -- Overrides the text color when the current cell is occupied by the cursor
+   cursor_fg = "#AFB5C1",
+   cursor_bg = "hsl(220, 17%, 35%)",
+   -- Specifies the border color of the cursor when the cursor style is set to Block,
+   -- or the color of the vertical or horizontal bar when the cursor style is set to
+   -- Bar or Underline.
+   cursor_border = "red",
+
+   -- the foreground color of selected text
+   selection_fg = "afb5c1",
+   -- the background color of selected text
+   selection_bg = "282c34",
+   -- When the IME, a dead key or a leader key are being processed and are effectively
+   -- holding input pending the result of input composition, change the cursor
+   -- to this color to give a visual cue about the compose state.
+   compose_cursor = "orange",
+
+   -- Colors for copy_mode and quick_select
+   -- available since: 20220807-113146-c2fee766
+   -- In copy_mode, the color of the active text is:
+   -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+   -- 2. selection_* otherwise
+   -- copy_mode_active_highlight_bg = { Color = "#000000" },
+   -- use `AnsiColor` to specify one of the ansi color palette values
+   -- (index 0-15) using one of the names "Black", "Maroon", "Green",
+   --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+   -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+   -- copy_mode_active_highlight_fg = { AnsiColor = "Black" },
+   -- copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
+   -- copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+   --
+   -- quick_select_label_bg = { Color = "peru" },
+   -- quick_select_label_fg = { Color = "#ffffff" },
+   -- quick_select_match_bg = { AnsiColor = "Navy" },
+   -- quick_select_match_fg = { Color = "#ffffff" },
 }
 
 -- general
