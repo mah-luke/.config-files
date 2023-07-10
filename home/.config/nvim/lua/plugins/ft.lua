78a6428d92a400c29bf68a166a5a9a0c6d3f93fa
Lukas <lukas.mah@outlook.at>
2023-07-10 19:19:25 +0200

    add vimtex plugin

--- home/.config/nvim/lua/plugins/ft.lua
+++ home/.config/nvim/lua/plugins/ft.lua
@@ -1,18 +1,59 @@
 -- Plugins which are filetype specific.
 
 return {
-  {
-    -- https://github.com/tridactyl/vim-tridactyl
-    "tridactyl/vim-tridactyl",
-    ft = "tridactyl",
-  },
-  {
-    -- https://github.com/iamcco/markdown-preview.nvim
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && yarn install",
-    ft = "markdown",
-    config = function()
-      vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
-    end,
-  },
+   {
+      -- https://github.com/tridactyl/vim-tridactyl
+      "tridactyl/vim-tridactyl",
+      ft = "tridactyl",
+   },
+   {
+      -- https://github.com/iamcco/markdown-preview.nvim
+      "iamcco/markdown-preview.nvim",
+      build = "cd app && yarn install",
+      ft = "markdown",
+      config = function()
+         vim.keymap.set("n", "<leader>md", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
+      end,
+   },
+   {
+      -- https://github.com/lervag/vimtex
+      "lervag/vimtex",
+      ft = "tex",
+      -- opts = {
+      --    vim.g.vimtex_view_general_viewer = "okular",
+      --
+      --    vim.g.vimtex_compiler_latexmk = {
+      --       aux_dir = "aux",
+      --       out_dir = "out",
+      --       callback = 1,
+      --       continuous = 1,
+      --       executable = "latexmk",
+      --       hooks = {},
+      --       -- \ 'options' : [
+      --       -- \   '-verbose',
+      --       -- \   '-file-line-error',
+      --       -- \   '-synctex=1',
+      --       -- \   '-interaction=nonstopmode',
+      --       -- \ ],
+      --    }
+      -- },
+      config = function()
+         vim.g.vimtex_compiler_latexmk = {
+            aux_dir = "aux",
+            out_dir = "out",
+            callback = 1,
+            continuous = 1,
+            executable = "latexmk",
+            hooks = {},
+            -- \ 'options' : [
+            -- \   '-verbose',
+            -- \   '-file-line-error',
+            -- \   '-synctex=1',
+            -- \   '-interaction=nonstopmode',
+            -- \ ],
+         }
+         vim.g.vimtex_view_general_viewer = "okular"
+         vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+      end,
+   },
 }
