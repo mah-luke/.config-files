31304dbb2cd1b7e36dc9c0b083f2f8d6a70cd1f4
Lukas <lukas.mah@outlook.at>
2023-07-10 19:37:48 +0200

    improve zathurarc and remove unneded opts comment.

--- home/.config/nvim/lua/plugins/ft.lua
+++ home/.config/nvim/lua/plugins/ft.lua
@@ -17,26 +17,9 @@ return {
    },
    {
       -- https://github.com/lervag/vimtex
+      -- pacman -S zathura xdotools
       "lervag/vimtex",
       ft = "tex",
-      -- opts = {
-      --    vim.g.vimtex_view_general_viewer = "okular",
-      --
-      --    vim.g.vimtex_compiler_latexmk = {
-      --       aux_dir = "aux",
-      --       out_dir = "out",
-      --       callback = 1,
-      --       continuous = 1,
-      --       executable = "latexmk",
-      --       hooks = {},
-      --       -- \ 'options' : [
-      --       -- \   '-verbose',
-      --       -- \   '-file-line-error',
-      --       -- \   '-synctex=1',
-      --       -- \   '-interaction=nonstopmode',
-      --       -- \ ],
-      --    }
-      -- },
       config = function()
          vim.g.vimtex_compiler_latexmk = {
             aux_dir = "aux",
@@ -52,8 +35,8 @@ return {
             -- \   '-interaction=nonstopmode',
             -- \ ],
          }
-         vim.g.vimtex_view_general_viewer = "okular"
-         vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+         vim.g.vimtex_view_general_viewer = "zathura"
+         -- vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
       end,
    },
 }
