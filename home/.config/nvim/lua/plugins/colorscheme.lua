c0c4833373211e31d16af991a319ca8e870763e9
Lukas Mahler <lukas.mah@outlook.at>
2023-07-14 16:01:24 +0200

    add fixme for term colors and add nvim diff

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,27 +5,27 @@
 -- TODO: comments more contrast
 
 return {
-   "navarasu/onedark.nvim",
-   lazy = false,
-   priority = 1000,
-   config = function()
-      require("onedark").setup({
-         -- style = "darker",
-         code_style = {
-            comments = "none",
-         },
-         transparent = true,
-         term_colors = true,
-         highlights = {
-            CursorLine = {
-               bg = "#1c1f24",--"#181a1f",
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("onedark").setup({
+            -- style = "darker",
+            code_style = {
+                comments = "none",
             },
-         },
-         -- colors = {
-         --    grey = "red",
-         -- },
-      })
-      vim.cmd.colorscheme("onedark")
-      vim.api.nvim_set_hl(0, "MatchParen", { bg = "#24282f",undercurl = true})
-   end,
+            transparent = true,
+            -- term_colors = true,
+            highlights = {
+                CursorLine = {
+                    bg = "#1c1f24", --"#181a1f",
+                },
+            },
+            -- colors = {
+            --    grey = "red",
+            -- },
+        })
+        vim.cmd.colorscheme("onedark")
+        vim.api.nvim_set_hl(0, "MatchParen", { bg = "#24282f", undercurl = true })
+    end,
 }
