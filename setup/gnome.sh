3d94c1a46e6e9bcb71deaa7225e163398c750e8e
Lukas Mahler <lukas.mah@outlook.at>
2023-10-12 12:03:44 +0200

    add gnome.sh

---
+++ setup/gnome.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# setting up gnome configuration as mentioned in gnome.md
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super><Alt>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super><Alt>Right']"
