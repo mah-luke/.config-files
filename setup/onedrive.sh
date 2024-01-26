a74228cfba13af4f31918222247909534d778483
Lukas Mahler <lukas.mah@outlook.at>
2024-01-26 12:32:33 +0100

    add echos to onedrive setup

--- setup/onedrive.sh
+++ setup/onedrive.sh
@@ -1,10 +1,12 @@
-#! /bin/sh
+#!/bin/sh
 
+echo "onedrive-TU"
 /usr/bin/onedrive --reauth --confdir=/home/lukas/.config/onedrive/onedrive-TU
 systemctl --user --now enable onedrive-TU
 
+echo "onedrive-PERS"
 /usr/bin/onedrive --reauth --confdir=/home/lukas/.config/onedrive/onedrive-PERS
 systemctl --user --now enable onedrive-PERS
 
-# follow logs with: 
+# follow logs with:
 # journalctl --user -f -u onedrive-<NAME>
