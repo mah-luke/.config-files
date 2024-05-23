e6aa8a4565f60c16e169690842b24c5bba338e3d
Lukas Mahler <lukas.mah@outlook.at>
2024-05-23 14:59:28 +0200

    feat: activate off_by_1 for molten as default

--- home/.config/nvim/lua/plugins/jupyter.lua
+++ home/.config/nvim/lua/plugins/jupyter.lua
@@ -182,7 +182,7 @@ _l_: run line  _R_: run above
             -- vim.g.molten_output_virt_lines = true
             vim.g.molten_virt_text_output = true
             -- vim.g.molten_use_border_highlights = true
-            -- vim.g.molten_virt_lines_off_by_1 = true
+            vim.g.molten_virt_lines_off_by_1 = true
             vim.g.molten_wrap_output = true
             vim.g.molten_tick_rate = 144
 
