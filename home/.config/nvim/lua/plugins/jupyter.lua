204479c612e77e41bcf5512b53ab5eff96f04afa
Lukas <lukas.mah@outlook.at>
2024-08-03 00:46:53 +0200

    feat: update image.nvim to use relative screen space

--- home/.config/nvim/lua/plugins/jupyter.lua
+++ home/.config/nvim/lua/plugins/jupyter.lua
@@ -17,10 +17,10 @@ return {
         },
         opts = {
             backend = "kitty", -- whatever backend you would like to use
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
+            -- max_width = 100,
+            -- max_height = 12,
+            max_height_window_percentage = 70, -- math.huge,
+            max_width_window_percentage = 70, -- math.huge,
             window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
             window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
 
