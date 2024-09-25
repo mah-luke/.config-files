13d98a8633555914978814e059bf34d40c9332f7
Lukas Mahler <lukas.mah@outlook.at>
2024-09-25 16:44:21 +0200

    chore: format init lsp file

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -66,48 +66,49 @@ return {
                 },
 
                 -- Rust
+                rust_analyzer = {},
 
                 -- C
                 clangd = {
-                keys = {
-                    {
-                        "<leader>ch",
-                        "<cmd>ClangdSwitchSourceHeader<cr>",
-                        desc = "Switch Source/Header (C/C++)",
+                    keys = {
+                        {
+                            "<leader>ch",
+                            "<cmd>ClangdSwitchSourceHeader<cr>",
+                            desc = "Switch Source/Header (C/C++)",
+                        },
+                    },
+                    root_dir = function(fname)
+                        return require("lspconfig.util").root_pattern(
+                            "Makefile",
+                            "configure.ac",
+                            "configure.in",
+                            "config.h.in",
+                            "meson.build",
+                            "meson_options.txt",
+                            "build.ninja"
+                        )(fname) or require("lspconfig.util").root_pattern(
+                            "compile_commands.json",
+                            "compile_flags.txt"
+                        )(fname) or require("lspconfig.util").find_git_ancestor(fname)
+                    end,
+                    capabilities = {
+                        offsetEncoding = { "utf-16" },
+                    },
+                    cmd = {
+                        "clangd",
+                        "--background-index",
+                        "--clang-tidy",
+                        "--header-insertion=iwyu",
+                        "--completion-style=detailed",
+                        "--function-arg-placeholders",
+                        "--fallback-style=LLVM",
+                    },
+                    init_options = {
+                        usePlaceholders = true,
+                        completeUnimported = true,
+                        clangdFileStatus = true,
                     },
                 },
-                root_dir = function(fname)
-                    return require("lspconfig.util").root_pattern(
-                        "Makefile",
-                        "configure.ac",
-                        "configure.in",
-                        "config.h.in",
-                        "meson.build",
-                        "meson_options.txt",
-                        "build.ninja"
-                    )(fname) or require("lspconfig.util").root_pattern(
-                        "compile_commands.json",
-                        "compile_flags.txt"
-                    )(fname) or require("lspconfig.util").find_git_ancestor(fname)
-                end,
-                capabilities = {
-                    offsetEncoding = { "utf-16" },
-                },
-                cmd = {
-                    "clangd",
-                    "--background-index",
-                    "--clang-tidy",
-                    "--header-insertion=iwyu",
-                    "--completion-style=detailed",
-                    "--function-arg-placeholders",
-                    "--fallback-style=LLVM",
-                },
-                init_options = {
-                    usePlaceholders = true,
-                    completeUnimported = true,
-                    clangdFileStatus = true,
-                },
-            },
 
                 -- Latex
                 ltex = {
--- home/.config/nvim/lua/plugins/rust.lua
+++ home/.config/nvim/lua/plugins/rust.lua
@@ -1,4 +1,4 @@
 return {
     "mrcjkb/rustaceanvim",
-    lazy = false,
+    lazy = true,
 }
