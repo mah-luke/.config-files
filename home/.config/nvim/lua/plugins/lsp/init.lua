c0cfe72960305bbd43a466dc102a0b40a66b1fa5
Lukas <lukas.mah@outlook.at>
2024-12-06 19:24:05 +0100

    feat: remove autocommand from none-ls

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -158,17 +158,17 @@ return {
             local nls = require("null-ls")
             return {
                 on_attach = function(client, bufnr)
-                    if client.supports_method("textDocument/formatting") then
-                        local augroup = vim.api.nvim_create_augroup("LspFromatting", { clear = true })
-                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                        vim.api.nvim_create_autocmd("BufWritePre", {
-                            group = augroup,
-                            buffer = bufnr,
-                            callback = function()
-                                vim.lsp.buf.format({ async = false })
-                            end,
-                        })
-                    end
+                    -- if client.supports_method("textDocument/formatting") then
+                    --     local augroup = vim.api.nvim_create_augroup("LspFromatting", { clear = true })
+                    --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    --     vim.api.nvim_create_autocmd("BufWritePre", {
+                    --         group = augroup,
+                    --         buffer = bufnr,
+                    --         callback = function()
+                    --             vim.lsp.buf.format({ async = false })
+                    --         end,
+                    --     })
+                    -- end
                 end,
                 sources = {
                     -- lua
