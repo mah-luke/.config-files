3222010b0519cd03fa1598914b2923710fd90c96
Lukas <lukas.mah@outlook.at>
2025-02-24 22:01:17 +0100

    fix: typo in lsp init

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -165,7 +165,7 @@ return {
             return {
                 on_attach = function(client, bufnr)
                     -- if client.supports_method("textDocument/formatting") then
-                    --     local augroup = vim.api.nvim_create_augroup("LspFromatting", { clear = true })
+                    --     local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
                     --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                     --     vim.api.nvim_create_autocmd("BufWritePre", {
                     --         group = augroup,
